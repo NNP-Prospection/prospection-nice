8edcc3b7b4798d59d80b1bca008be749095c37c8
NNP-Prospection <nathalie.par@gmail.com>
2026-09-26 18:35:56 +0200

    Update Accueil.py

--- Accueil.py
+++ Accueil.py
@@ -239,14 +239,13 @@ def generer_listing_maitre_strict(obj, sect, budget):
 if lancer:
     st.info(f"Génération de la base certifiée pour : **{objectif}** sur le secteur **{secteur}**...")
     
-    df_resultats = generer_listing_maitre_strict(objectif, secteur, budget_max)
-    
-    if len(df_resultats) > 0:
+     df_resultats = generer_listing_maitre_strict(objectif, secteur, budget_max)
+     if len(df_resultats) > 0:
       # On applique l'enrichissement officiel si l'objectif concerne les passoires énergétiques
 if "Passoires" in str(objectif):
-    df_resultats = enrichir_sci_avec_sirene(df_resultats)  
-        st.success(f"🎯 **{len(df_resultats)} biens uniques et vérifiés** trouvés pour cet objectif !")
-        st.dataframe(df_resultats, use_container_width=True)
+    df_resultats = enrichir_sci_avec_sirene(df_resultats)         
+st.success(f"🎯 **{len(df_resultats)} biens uniques et vérifiés** trouvés pour cet objectif !")
+st.dataframe(df_resultats, use_container_width=True)
         
         csv = df_resultats.to_csv(index=False).encode('utf-8')
         st.download_button(
